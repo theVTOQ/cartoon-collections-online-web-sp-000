1a3ea8b60e23cb6816294b423ecbcfcdddf011db
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 18:27:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index {|dwarf, index| puts "#{index +  1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.each {|call| puts "#{call.capitalize!}!"}
 end
 
 def long_planeteer_calls# code an argument here
