c607480d81c8f80fe0d8fa093b3be3db27e64336
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 18:28:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(calls)# code an argument here
   # Your code here
-  calls.each {|call| puts "#{call.capitalize!}!"}
+  calls.collect {|call| "#{call.capitalize!}!"}
 end
 
 def long_planeteer_calls# code an argument here
